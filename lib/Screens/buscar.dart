ec1ca82ff9488ebfd98eb02afe906e04b9d2c991
FabiolaLainez <167152904+FabiolaLainez@users.noreply.github.com>
2024-05-23 22:46:40 -0600

    BottomNavigationBar

--- lib/Screens/buscar.dart
+++ lib/Screens/buscar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BuscarScreen extends StatefulWidget {
   const BuscarScreen({super.key});
@@ -8,10 +9,57 @@ class BuscarScreen extends StatefulWidget {
 }
 
 class _BuscarScreenState extends State<BuscarScreen> {
+   int _currentIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+      switch (index) {
+        case 0:
+          context.go('/home');
+          break;
+        case 1:
+          context.go('/ListaBebidas');
+          break;
+        case 2:
+          context.go('/Buscar');
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Buscar'),
+      appBar: AppBar(
+        title: const Text(
+          'Cocktails',
+          style: TextStyle(
+              color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.red,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: Center(
+        child: Text('Buscador de bebidas'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Lista de bebidas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Buscar',
+          ),
+        ],
+      ),
     );
   }
 }
